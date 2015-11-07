02e27c04255106c8b023b638d7455c10b0d9c919
KeitIG <pierre.de.la.martiniere@gmail.com>
2015-11-07 19:20:08 +0100

    Pre-release

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express'),
+    stylus  = require('stylus'),
+    path    = require('path');
+
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'src', 'views'));
+app.set('view engine', 'jade');
+app.use(stylus.middleware({
+    src     : path.join(__dirname, 'src', 'styles'),
+    dest    : path.join(__dirname, 'public'),
+    compile : function (str, path) { // optional, but recommended
+         return stylus(str)
+            .set('filename', path)
+            .set('compress', true);
+    }
+}));
+app.use(express.static(__dirname + '/public'))
+
+
+
+/*--- Routes -*/
+app.get('/', function (req, res) {
+    res.render(
+        'home',
+        { title : 'Home' }
+    )
+})
+
+app.listen(3000)
