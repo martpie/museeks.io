44d1cb4deaa8f5ad2cc11d62b16a9afe510c9d12
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2019-07-19 18:58:13 +0200

    Fix gulp 4 migration

--- gulpfile.js
+++ gulpfile.js
@@ -3,71 +3,65 @@ const pug = require('gulp-pug');
 const stylus = require('gulp-stylus');
 const del = require('del');
 
-
 /**
-* Main tasks
-*/
+ * Main tasks
+ */
 
 gulp.task('clean', () => {
-  return del([
-    'dist/**/*',
-    '!dist/mobile/.keep',
-  ]);
+  return del(['dist/**/*', '!dist/mobile/.keep']);
 });
 
 gulp.task('root', () => {
-  return gulp.src('src/*')
-    .pipe(gulp.dest('./dist/'));
+  return gulp.src('src/*').pipe(gulp.dest('./dist/'));
 });
 
-
 gulp.task('views', () => {
-  return gulp.src(['src/views/*.pug', '!src/views/layout.pug'])
+  return gulp
+    .src(['src/views/*.pug', '!src/views/layout.pug'])
     .pipe(pug({}))
     .pipe(gulp.dest('./dist/'));
 });
 
-
 gulp.task('styles', () => {
-  return gulp.src('src/styles/main.styl')
-  .pipe(stylus())
+  return gulp
+    .src('src/styles/main.styl')
+    .pipe(stylus())
     .pipe(gulp.dest('./dist/styles'));
 });
 
-
 gulp.task('scripts', () => {
-  return gulp.src('src/scripts/*.js')
-    .pipe(gulp.dest('./dist/scripts'));
+  return gulp.src('src/scripts/*.js').pipe(gulp.dest('./dist/scripts'));
 });
 
 gulp.task('images', () => {
-  return gulp.src('src/images/*')
-    .pipe(gulp.dest('./dist/images'));
+  return gulp.src('src/images/*').pipe(gulp.dest('./dist/images'));
 });
 
 gulp.task('fonts', () => {
-  return gulp.src('src/fonts/*')
-    .pipe(gulp.dest('./dist/fonts'));
+  return gulp.src('src/fonts/*').pipe(gulp.dest('./dist/fonts'));
 });
 
-
 /**
-* Default tasks
-*/
+ * Default tasks
+ */
 
-gulp.task('default', ['root', 'views', 'styles', 'scripts', 'images', 'fonts']);
+gulp.task(
+  'default',
+  gulp.series('root', 'views', 'styles', 'scripts', 'images', 'fonts')
+);
 
 gulp.task('dev', () => {
   const pugWatcher = gulp.watch('src/**/*', ['default']);
 
   pugWatcher.on('change', handleFileChange);
-})
-
+});
 
 /**
-* Utils
-*/
+ * Utils
+ */
 
 const handleFileChange = function(event) {
-  console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-}
+  console.log(
+    'File ' + event.path + ' was ' + event.type + ', running tasks...'
+  );
+};
